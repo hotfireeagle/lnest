af02525f431b5a13d1f7a77a0fffaf1c4718bf03
hotfireeagle <hotfireeagle@163.com>
2019-12-12 22:53:28 +0800

    删除测试文件

--- src/common/class-validate-consts.ts
+++ src/common/class-validate-consts.ts
@@ -1,5 +1,9 @@
 export const EMPTY_NAME = '用户名不能为空';
 export const NAME_NON_STRING = '用户名类型必须为字符串';
+export const NAME_SHOULD_MORE_THEN_1 = '用户名不得少于1位';
+export const NAME_SHOULD_LESS_THEN_8 = '用户名不得超过8位';
 
 export const EMPTY_PASSWORD = '密码不能为空';
 export const PASSWORD_NON_STRING = '密码类型必须为字符串';
+export const PASSWORD_SHOULD_MORE_THEN_6 = '密码不得少于6位';
+export const PASSWORD_SHOULD_LESS_THEN_12 = '密码不得超过12位';
--- src/module/user/dto/user-register.dto.ts
+++ src/module/user/dto/user-register.dto.ts
@@ -1,12 +1,19 @@
-import { IsString, IsNotEmpty } from 'class-validator';
-import { EMPTY_NAME, EMPTY_PASSWORD, NAME_NON_STRING, PASSWORD_NON_STRING } from '../../../common/class-validate-consts';
+import { IsString, IsNotEmpty, MinLength, MaxLength } from 'class-validator';
+import {
+  EMPTY_NAME, EMPTY_PASSWORD, NAME_NON_STRING, PASSWORD_NON_STRING,
+  NAME_SHOULD_MORE_THEN_1, NAME_SHOULD_LESS_THEN_8, PASSWORD_SHOULD_MORE_THEN_6, PASSWORD_SHOULD_LESS_THEN_12,
+} from '../../../common/class-validate-consts';
 
 export class CreateUserDto {
   @IsNotEmpty({ message: EMPTY_NAME })
   @IsString({ message: NAME_NON_STRING })
+  @MinLength(1, { message: NAME_SHOULD_MORE_THEN_1 })
+  @MaxLength(8, { message: NAME_SHOULD_LESS_THEN_8 })
   readonly name: string;
 
   @IsNotEmpty({ message: EMPTY_PASSWORD })
   @IsString({ message: PASSWORD_NON_STRING })
+  @MaxLength(12, { message: PASSWORD_SHOULD_LESS_THEN_12 })
+  @MinLength(6, { message: PASSWORD_SHOULD_MORE_THEN_6 })
   readonly password: string;
 }
